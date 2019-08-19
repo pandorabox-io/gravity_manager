5d500e1a036241c4628eda053242a0fa219c5362
coil <51716565+coil0@users.noreply.github.com>
2019-08-19 19:39:47 -0400

    Use player_monoids if available

--- timer.lua
+++ timer.lua
@@ -1,28 +1,37 @@
 local has_armor_mod = minetest.get_modpath("3d_armor")
+local use_player_monoids = minetest.global_exists("player_monoids")
 
 local timer = 0
 
 function update_gravity(player)
 	local pos = player:get_pos()
-	local name = player:get_player_name()
 
 	if not pos then
 		-- yeah, it happens apparently :)
 		return
 	end
 
-	local phys_override = player:get_physics_override()
-	local current_gravity = phys_override.gravity
 	local new_gravity = gravity_manager.get_gravity(pos)
+	if use_player_monoids then
+		if new_gravity then
+			player_monoids.gravity:add_change(player, new_gravity, "gravity_manager:gravity")
+		else
+			player_monoids.gravity:del_change(player, "gravity_manager:gravity")
+		end
+	else
+		local phys_override = player:get_physics_override()
+		local current_gravity = phys_override.gravity
 
-	if math.abs(current_gravity - new_gravity) > 0.01 then
-		minetest.log("action", "[gravity_manager] setting new gravity " .. new_gravity ..
-			" for player: " .. name)
-		player:set_physics_override({gravity=new_gravity})
+		if math.abs(current_gravity - new_gravity) > 0.01 then
+			local name = player:get_player_name()
+			minetest.log("action", "[gravity_manager] setting new gravity " .. new_gravity ..
+				" for player: " .. name)
+			player:set_physics_override({gravity=new_gravity})
+		end
 	end
 end
 
-if has_armor_mod then
+if has_armor_mod and not use_player_monoids then
 	-- update physics if armor changed
 	armor:register_on_update(update_gravity)
 end
